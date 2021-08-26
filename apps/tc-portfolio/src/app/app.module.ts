15add6b6e566325cdd0fc6508e82dc48c08efdb9
unknown <theo.canonne99@laposte.net>
2021-08-26 23:39:09 +0200

    removing bootstrap

--- apps/tc-portfolio/src/app/app.module.ts
+++ apps/tc-portfolio/src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-
 export class AppModule {}
--- apps/tc-portfolio/src/app/pages/home/pages/home/home.component.ts
+++ apps/tc-portfolio/src/app/pages/home/pages/home/home.component.ts
@@ -3,21 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'portfolio-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   projectsList: { imageName?: string; desc: string; link: string }[] = [
-    { imageName: 'raliraid-logo.png', desc: 'Site pour une association participant au 4L Trophy.' , link: 'http://raliraid.com/'},
-    { imageName: 'emothep-logo.png', desc: 'Site d\'une entreprise de services informatiques.', link: 'https://e-mothep.fr/' }
+    {
+      imageName: 'raliraid-logo.png',
+      desc: 'Site pour une association participant au 4L Trophy.',
+      link: 'http://raliraid.com/',
+    },
+    {
+      imageName: 'emothep-logo.png',
+      desc: 'Site d\'une entreprise de services informatiques.',
+      link: 'https://e-mothep.fr/',
+    },
+    {
+      imageName: 'fem-ecommerce.png',
+      desc: 'Intégration d\'une maquette Front End Mentor. Page d\'accueil d\'un site e-commerce ',
+      link: 'https://fem.ecommerce.tc-dev.tech/',
+    }
   ];
 
-  technologiesList: { images?: {path: string; title: string}[]; numberStars: number; }[] = [
-    { images: [{path: 'angular-logo.png', title: 'Angular'}, {path: 'nodejs-logo.png', title: 'NodeJS'},
-    {path: 'github-logo.png', title: 'Git'}], numberStars: 3 },
-    { images: [{path: 'firebase-logo.png', title: 'Firebase'}, {path: 'linux-logo.png', title: 'Linux'},
-    {path: 'nginx-logo.png', title: 'Nginx'}], numberStars: 2 },
-    { images: [{path: 'docker-logo.png', title: 'Docker'}, {path: 'postgresql-logo.png', title: 'Postgresql - SQL'},
-    {path: 'software-ag-logo.png', title: 'Software AG'}], numberStars: 1 }
+  technologiesList: {
+    images?: { path: string; title: string }[];
+    numberStars: number;
+  }[] = [
+    {
+      images: [
+        { path: 'angular-logo.png', title: 'Angular' },
+        { path: 'nodejs-logo.png', title: 'NodeJS' },
+        { path: 'github-logo.png', title: 'Git' },
+      ],
+      numberStars: 3,
+    },
+    {
+      images: [
+        { path: 'firebase-logo.png', title: 'Firebase' },
+        { path: 'linux-logo.png', title: 'Linux' },
+        { path: 'nginx-logo.png', title: 'Nginx' },
+      ],
+      numberStars: 2,
+    },
+    {
+      images: [
+        { path: 'docker-logo.png', title: 'Docker' },
+        { path: 'postgresql-logo.png', title: 'Postgresql - SQL' },
+        { path: 'software-ag-logo.png', title: 'Software AG' },
+      ],
+      numberStars: 1,
+    },
   ];
-
 }
